b76f20a1147a53b00e25cdf4058e7b1ad921e7c2
shohaul <shihabshohaul143@gmail.com>
2021-07-26 14:01:48 +0600

    added reusable form input

--- pages/search-donors/index.js
+++ pages/search-donors/index.js
@@ -1,7 +1,15 @@
+import DarkMode from "@components/DarkMode"
+import FormInput from "@components/others/FormInput"
+import { AiFillHeart } from "react-icons/ai"
+
 export default function SearchDonorsPage() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <section>
+      <DarkMode />
+      <h1 className="text-3xl font-bold mt-5 ml-5">Search Donors</h1>
+      <div>
+        <FormInput Icon={AiFillHeart} />
+      </div>
+    </section>
   )
 }
