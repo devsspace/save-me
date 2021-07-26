7b8b578eca1baae4501ffc4f3044074e4e490c99
Abdify <abdullah.fahimm08@gmail.com>
2021-07-26 15:29:05 +0600

    dark mode updated, form input updated of signup page

--- pages/user/signup/index.js
+++ pages/user/signup/index.js
@@ -1,22 +1,25 @@
 import DarkMode from "@components/DarkMode"
 import AppButton from "@components/others/AppButton"
+import FormInput from "@components/others/FormInput"
 import Link from "next/link"
 import React, { useState } from "react"
+import { useForm } from "react-hook-form"
 import classes from "../login/login.module.css"
 
 const index = () => {
   return (
-    <div className={`bg-primary dark:bg-dark ${classes.login}`}>
+    <div className={`bg-light dark:bg-dark ${classes.login}`}>
       <DarkMode />
       <h1>Join Now</h1>
       <LogInForm />
-      <small>
+      <small className="text-blue-400">
         <Link href="/user/login">Already have an account?</Link>
       </small>
     </div>
   )
 
   function LogInForm() {
+    const { register, handleSubmit } = useForm()
     const initialState = {
       email: "",
       password: "",
@@ -30,8 +33,8 @@ const index = () => {
       setUser({ ...user, [e.target.name]: e.target.value })
     }
 
-    async function handleLogIn(e) {
-      e.preventDefault()
+    async function handleLogIn(data) {
+      // e.preventDefault()
       // setLoading(true)
 
       // try {
@@ -47,10 +50,11 @@ const index = () => {
       //   setError(err.message)
       //   setLoading(false)
       // }
+      console.log(data)
     }
 
     return (
-      <form onSubmit={handleLogIn} className={classes.loginForm}>
+      <form onSubmit={handleSubmit(handleLogIn)} className={classes.loginForm}>
         {loading && (
           <div className="loading">
             <h1>Loading...</h1>
@@ -58,7 +62,18 @@ const index = () => {
         )}
         {error && <p className="error-text">{error}</p>}
 
-        <input
+        <FormInput name="email" type="email" placeholder="Email" register={register} />
+        <FormInput name="password" type="password" placeholder="Password" register={register} />
+        <FormInput name="confirmPassword" type="password" placeholder="Confirm Password" register={register} />
+        {/* <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={user.email}
+          onChange={handleChange}
+        />
+        <FormInput
           type="email"
           name="email"
           placeholder="Email"
@@ -74,8 +89,8 @@ const index = () => {
           value={user.password}
           pattern=".{6,}"
           onChange={handleChange}
-          onInvalid={(e) => e.target.setCustomValidity("Do you tell that a password!?")}
-          onInput={(e) => e.target.setCustomValidity("")}
+          onInvalid={e => e.target.setCustomValidity("Do you tell that a password!?")}
+          onInput={e => e.target.setCustomValidity("")}
         />
         <input
           type="password"
@@ -84,7 +99,7 @@ const index = () => {
           required
           value={user.confirmPassword}
           onChange={handleChange}
-        />
+        /> */}
         <AppButton>Sign Up</AppButton>
       </form>
     )
