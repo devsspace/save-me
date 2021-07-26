1b26f5b3ece6a2d17eea232c5d8a57969f12b30f
Abdify <abdullah.fahimm08@gmail.com>
2021-07-26 14:05:47 +0600

    working on signup page

---
+++ pages/user/signup/index.js
@@ -0,0 +1,92 @@
+import AppButton from "@components/others/AppButton"
+import Link from "next/link"
+import React, { useState } from "react"
+import classes from "../login/login.module.css"
+
+const index = () => {
+  return (
+    <div className={classes.login}>
+      <h1>Join Now</h1>
+      <LogInForm />
+      <small>
+        <Link href="/user/login">Already have an account?</Link>
+      </small>
+    </div>
+  )
+
+  function LogInForm() {
+    const initialState = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    }
+    const [user, setUser] = useState(initialState)
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const handleChange = (e) => {
+      setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    async function handleLogIn(e) {
+      e.preventDefault()
+      // setLoading(true)
+
+      // try {
+      //   const { data } = await signUp(user)
+
+      //   if (!data.user) {
+      //     setError(data.message)
+      //     setLoading(false)
+      //     return
+      //   }
+      //   // history.push("/login")
+      // } catch (err) {
+      //   setError(err.message)
+      //   setLoading(false)
+      // }
+    }
+
+    return (
+      <form onSubmit={handleLogIn} className={classes.loginForm}>
+        {loading && (
+          <div className="loading">
+            <h1>Loading...</h1>
+          </div>
+        )}
+        {error && <p className="error-text">{error}</p>}
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={user.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={user.password}
+          pattern=".{6,}"
+          onChange={handleChange}
+          onInvalid={(e) => e.target.setCustomValidity("Do you tell that a password!?")}
+          onInput={(e) => e.target.setCustomValidity("")}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          required
+          value={user.confirmPassword}
+          onChange={handleChange}
+        />
+        <AppButton text="Sign up" />
+      </form>
+    )
+  }
+}
+
+export default index
