89e2ec6f4f8024ac2a15a7055e417fc900f6e05a
shohaul <shihabshohaul143@gmail.com>
2021-07-27 11:55:07 +0600

    added svg image

--- pages/search-donors/index.js
+++ pages/search-donors/index.js
@@ -3,6 +3,7 @@ import AppButton from "@components/others/AppButton"
 import AppDatePicker from "@components/others/AppDatePicker"
 import AppDropdown from "@components/others/AppDropdown"
 import districtsData from "@configs/fakeData/districts"
+import Image from "next/image"
 import { HiSearch } from "react-icons/hi"
 
 export default function SearchDonorsPage() {
@@ -16,18 +17,26 @@ export default function SearchDonorsPage() {
     { id: 7, name: "AB+" },
     { id: 8, name: "AB-" },
   ]
+  const donors = [
+    { id: 2, name: "Eligible" },
+    { id: 1, name: "All" },
+  ]
   return (
-    <section className="container mx-auto">
+    <section className="container mx-auto p-5 md:p-0">
       <DarkMode />
       <h1 className="text-3xl font-bold mt-5 mb-2">Search Donors</h1>
-      <div>
-        <div />
-
-        <div className="flex space-x-4">
-          <AppDropdown data={bloodGroups} />
-          <AppDropdown data={districtsData} />
-          <AppDatePicker />
-          <AppButton Icon={HiSearch}>Search</AppButton>
+      <div className="flex justify-between">
+        <AppDropdown data={bloodGroups} />
+        <AppDropdown data={districtsData} />
+        <AppDatePicker />
+        <AppDropdown data={districtsData} />
+        <AppButton className="h-9" Icon={HiSearch} />
+        <div className="hidden md:block">
+          <Image
+            src="/images/searchingBloodDonor.svg"
+            height={450}
+            width={450}
+          />
         </div>
       </div>
     </section>
