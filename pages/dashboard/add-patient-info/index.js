479103d71f809d3cfc78a753cdbd21188fc194c3
Samayun Chowdhury <samayun.m.chowdhury.com>
2021-07-28 20:22:46 +0600

    /dashboard/add-patient-info added

--- pages/dashboard/add-patient-info/index.js
+++ pages/dashboard/add-patient-info/index.js
@@ -1,7 +1,19 @@
+import BloodNavBar from "@components/bloodManagement/BloodNavBar"
+import MultiStepForm from "@components/MultiStepForm/MultiStepForm"
 import React from "react"
 
-const index = () => {
-  return <div />
-}
+const DonorProfile = () => (
+  <>
+    <BloodNavBar />
+    <div>
+      <div className="h-24 min-h-0 md:min-h-full flex items-center">
+        <div className="container mx-auto">
+          <h1 className="text-2xl"> Patient Info </h1>
+        </div>
+      </div>
+      <MultiStepForm />
+    </div>
+  </>
+)
 
-export default index
+export default DonorProfile
