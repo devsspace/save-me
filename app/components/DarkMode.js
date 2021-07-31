c261aa66eff92278460e40a3eddc741fd0e23cdb
Shohaul <shihabshohaul143@gmail.com>
2021-07-31 21:21:17 +0600

    fixed warnings: content did not match on client & unique key prop for map method (in array)

--- app/components/DarkMode.js
+++ app/components/DarkMode.js
@@ -7,6 +7,7 @@ export default function DarkMode() {
       <button
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         className="dark:text-light"
+        suppressHydrationWarning
       >
         Turn {theme === "dark" ? "On" : "Off"} Lights
       </button>
