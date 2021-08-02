d27a54c7f8cbe5c7bcf8d1c1adec08a7435e5386
miyadhossain <miyad.mh@gmail.com>
2021-08-02 14:25:36 +0600

    donor info

--- app/components/MultiStepForm/DonorInfo.js
+++ app/components/MultiStepForm/DonorInfo.js
@@ -5,10 +5,6 @@ const DonorInfo = () => {
     <div className="mx-auto max-w-6xl p-12">
       <div className="flex flex-col md:flex-row justify-center">
         <div className="md:w-1/2 max-w-md flex flex-col justify-center">
-          {/* <div className="md:text-4xl text-xl text-primary uppercase">
-            We Save Lives
-          </div>
-          <div className="text-xl mt-4">Welcome, You're a Real Hero</div> */}
           <img
             className=""
             src="https://res.cloudinary.com/dxvzhnyuo/image/upload/v1627829528/blood-donor-removebg-preview_snnwvx.png"
@@ -16,7 +12,7 @@ const DonorInfo = () => {
           />
         </div>
         <div className="md:w-1/2 flex justify-start mt-5 md:justify-end w-full md:w-1/2 ">
-          <div className="shadow-md flex-auto max-w-sm p-10 pb-20">
+          <div className="shadow-md flex-auto max-w-sm p-10 pb-20 w-96">
             <div className="w-full">
               <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase">
                 <span className="text-red-400 mr-1">*</span> Full Name
@@ -25,7 +21,7 @@ const DonorInfo = () => {
                 <input
                   placeholder="Jhon"
                   className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
-                />{" "}
+                />
               </div>
             </div>
             <div className="w-full">
