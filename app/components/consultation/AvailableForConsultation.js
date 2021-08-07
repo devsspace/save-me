0429fd0d20064239c8116227c837278e4860e498
Shohaul <shihabshohaul143@gmail.com>
2021-08-07 18:15:01 +0600

    added useRatingClient custom hook for reusablility and  available for consultation component 2/3 done

--- app/components/consultation/AvailableForConsultation.js
+++ app/components/consultation/AvailableForConsultation.js
@@ -6,9 +6,11 @@ export default function AvailableForConsultation() {
   return (
     <AppContainer>
       <div className="flex justify-between">
-        <h1 className="text-xl font-bold">Available For Consultation Now</h1>
+        <h1 className="text-xs sm:text-xl font-bold">
+          Available For Consultation Now
+        </h1>
         <div className="flex items-center">
-          <button className="font-poppins text-xl font-bold mr-1">
+          <button className="font-poppins text-xs  sm:text-xl font-bold mr-1">
             View All
           </button>
           <TiArrowRightThick />
---
+++ app/configs/fakeData/doctors.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    id: "1",
+    gender: "male",
+    firstName: "Mahbubul",
+    lastName: "Alam",
+    isOnline: true,
+    email: "test@test.com",
+    mobile: "01950743855",
+    dob: "1976-12-15",
+    worksAt: "ENT, General Physician, Covid Unit",
+    photoURL:
+      "https://images.unsplash.com/photo-1559839734-2b71ea197ec2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    profileDesc:
+      "Dr Mahububul Alam Kulkarni belives that once you start being more aware of your thoughts and actions, you will start appreciating life in a new way. This is why he specializes in depression, mood disorders, sexual disorders and anxiety disorders, because he feels that every person is capable of bringing about a change from within. His process is supportive, but aimed at direct interaction to help the patient bring about a visible change in himself or herself.",
+    specializations: ["Anger Management", "Depression", "Domestic Violence"],
+    license: [
+      "Registration No. MMC 2009093394, Medical Council of India 09/33958",
+      "2006113358 Maharashtra Medical Council, 2006",
+    ],
+    language: ["Hindi", " English", " Telugu", "Hagu"],
+    experience: [
+      {
+        company: "Apollo Hospital chennai",
+        years: "10 years",
+        desc: "Gt when then formula was invented",
+        awards: "This is the test award ",
+      },
+      {
+        company: "Apollo Hospital Mumbai",
+        years: "15 years",
+        desc: "Gt when then formula was Used",
+        awards: "This is the test award -2",
+      },
+    ],
+    communication: [
+      {
+        mode: "Audio",
+        cost: 600,
+        currency: "INR",
+        icon: "telephone.png",
+        location: "-1",
+      },
+      {
+        mode: "Video",
+        cost: 800,
+        currency: "INR",
+        icon: "video.png",
+        location: "-1",
+      },
+      {
+        mode: "Chat",
+        cost: 300,
+        currency: "INR",
+        icon: "chat.png",
+        location: "-1",
+      },
+    ],
+    conversations: 946,
+    rating: 2.5,
+  },
+]
