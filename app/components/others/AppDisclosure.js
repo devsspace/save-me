b8531e896a3556401023e1dc56fde9534f14201d
Shohaul <shihabshohaul143@gmail.com>
2021-08-07 22:34:54 +0600

    modified FAQs component styles to look good in dark mode

--- app/components/others/AppDisclosure.js
+++ app/components/others/AppDisclosure.js
@@ -3,11 +3,11 @@ import { HiChevronDown } from "react-icons/hi"
 
 export default function AppDisclosure({ question, answer }) {
   return (
-    <div className="w-full max-w-md p-1 mx-auto bg-light rounded-md">
+    <div className="w-full max-w-md p-1 dark:p-0 mx-auto bg-light rounded-md">
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left bg-primaryLight dark:bg-gray-600 rounded-md group hover:bg-primary focus:outline-none focus-visible:ring-primary focus-visible:ring-opacity-75">
+            <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left bg-primaryLight dark:bg-gray-600 rounded-md group hover:bg-primary dark:hover:bg-primary focus:outline-none focus-visible:ring-primary focus-visible:ring-opacity-75">
               <span className="group-hover:text-white">{question}</span>
               <HiChevronDown
                 className={`${
