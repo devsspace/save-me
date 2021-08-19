590ea77f6f5e264851b92ec6877953aa9455d49c
Abdify <abdullah.fahimm08@gmail.com>
2021-08-19 19:45:04 +0600

    server url updated

--- app/api/index.js
+++ app/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const url = "https://save-me-dev.herokuapp.com"
-const API = axios.create({ baseURL: "http://localhost:5000" })
+const API = axios.create({ baseURL: url })
 
 API.interceptors.request.use((req) => {
   // if (localStorage?.getItem("profile")) {
@@ -20,7 +20,7 @@ API.interceptors.request.use((req) => {
 export const logIn = (user) => API.post("/user/login", user)
 export const signUp = (user) => API.post("/user/signup", user)
 export const getUser = () => API.get("/user")
-export const saveProfile = (profileInfo) => API.put(`/user`, profileInfo)
+export const saveProfile = (profileInfo) => API.put("/user", profileInfo)
 
 export const searchDonor = (searchInfo) => {
   if (!searchInfo) return API.get("/donors")
@@ -38,8 +38,8 @@ export const getDonor = (donorId) => API.get(`/donors/${donorId}`)
 export const askDonation = (info) => API.post("/donation", info)
 
 export const requestBlood = (bloodReqInfo) =>
-  API.post(`/request-blood`, bloodReqInfo)
-export const getRequests = () => API.get(`/request-blood`)
+  API.post("/request-blood", bloodReqInfo)
+export const getRequests = () => API.get("/request-blood")
 export const updateRequest = (requestInfo) => API.put(`/request-blood/${requestInfo._id}`, requestInfo)
 
 
@@ -61,5 +61,5 @@ export const updateDonation = (donationId, donationInfo) =>
 
 
   export const getWaitingList = (doctorId) => API.get(`/consult/${doctorId}`)
-  export const getDoctors = () => API.get(`/doctors`)
+  export const getDoctors = () => API.get("/doctors")
   export const getDoctor = (doctorId) => API.get(`/doctors/${doctorId}`)
