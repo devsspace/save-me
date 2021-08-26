0f92c4935fc15769a669b6967cd4bf935cc6921b
Abdify <abdullah.fahimm08@gmail.com>
2021-08-26 15:23:58 +0600

    doctor can now set complete or not fonud status for patients in the waiting list, payment process dynamic

--- app/components/video/Sidebar.js
+++ app/components/video/Sidebar.js
@@ -5,13 +5,22 @@ import { useUserContext } from "app/contexts/UserContext"
 import { SocketContext } from "app/contexts/videoContext"
 import { useRouter } from "node_modules/next/dist/client/router"
 import React, { useContext, useEffect, useState } from "react"
+import { MdCancel } from "react-icons/md"
+import { TiTick } from "react-icons/ti"
 
 const Sidebar = ({ children }) => {
-  const { callAccepted, name, setName, callEnded, leaveCall, callUser, socket } =
-    useContext(SocketContext)
+  const {
+    callAccepted,
+    name,
+    setName,
+    callEnded,
+    leaveCall,
+    callUser,
+    socket,
+  } = useContext(SocketContext)
   const [patients, setPatients] = useState([])
   const { currentUser } = useUserContext()
-  
+
   const router = useRouter()
   const { doctorId } = router.query
   const isDoctor = currentUser._id === doctorId
@@ -25,28 +34,33 @@ const Sidebar = ({ children }) => {
         console.log(error)
       }
     }
-    if(isDoctor) get()
+    if (isDoctor) get()
   }, [])
 
-    socket.on("patient-added", (newPatient) => {
-      setPatients([...patients, newPatient])
-     
-    })
-    socket.on("patient-removed", (patientId) => {
-      setPatients(patients?.filter(p => p.patientId !== patientId))
-    })
+  socket.on("patient-added", (newPatient) => {
+    setPatients([...patients, newPatient])
+  })
+  socket.on("patient-removed", (patientId) => {
+    setPatients(patients?.filter((p) => p.patientId !== patientId))
+  })
+
+  const handleCancel = (patientId) => {
+    socket.emit("remove-patient", patientId, patients?.length, true)
+  }
 
   return (
     <div className="w-[90%] lg:w-1/2 my-10 mx-auto">
       {callAccepted && !callEnded && (
-        <AppButton className="justify-center bg-error my-5" onClick={leaveCall}>End Call</AppButton>
+        <AppButton className="justify-center bg-error my-5" onClick={leaveCall}>
+          End Call
+        </AppButton>
       )}
       {patients.length ? (
         patients.map((patient, i) => (
           <Table>
             <tr>
               <TD>
-                <span>{i+1}</span>
+                <span>{i + 1}</span>
               </TD>
               <TD className="w-1/2">
                 <span>{patient.name}</span>
@@ -61,6 +75,23 @@ const Sidebar = ({ children }) => {
                   Call
                 </AppButton>
               </TD>
+
+              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <span className="flex">
+                  <a
+                    className="text-red-600 hover:bg-red-700 hover:text-red-200 rounded-full border border-red-600 py-1 px-2 mr-2 cursor-pointer"
+                    onClick={() => handleCancel(patient.patientId)}
+                  >
+                    <MdCancel />
+                  </a>
+                  <a
+                    className="text-green-600 hover:bg-green-700 hover:text-green-100 rounded-full border border-green-600 py-1 px-2 cursor-pointer"
+                    onClick={() => handleCancel(patient.patientId)}
+                  >
+                    <TiTick />
+                  </a>
+                </span>
+              </td>
             </tr>
           </Table>
         ))
