3bf08be55443c8a0c656174bad2cd3c790695390
Abdify <abdullah.fahimm08@gmail.com>
2021-08-28 19:51:26 +0600

    video chat bug

--- app/contexts/videoContext.js
+++ app/contexts/videoContext.js
@@ -87,20 +87,12 @@ const VideoContextProvider = ({ children }) => {
     setCallAccepted(true)
 
     const peer = new Peer({
-      initiator: true,
+      initiator: false,
       trickle: false,
       config: {
         iceServers: [
-          {
-            urls: "stun:numb.viagenie.ca",
-            username: "sultan1640@gmail.com",
-            credential: "98376683",
-          },
-          {
-            urls: "turn:numb.viagenie.ca",
-            username: "sultan1640@gmail.com",
-            credential: "98376683",
-          },
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:global.stun.twilio.com:3478?transport=udp" },
         ],
       },
       stream,
